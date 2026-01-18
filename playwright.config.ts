298f0fbe2c1eb639e5ae358619fc8961afa2af51
nickiovets <indicow@gmail.com>
2026-01-18 13:03:03 +0100

    add headers

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,15 @@ export default defineConfig({
     trace: 'retain-on-failure',
     headless: env.CI,
     baseURL: env.BASE_URL,
+    locale: 'en-US',
+    timezoneId: 'Europe/Amsterdam',
+    extraHTTPHeaders: {
+      'Accept-Language': 'en-US,en;q=0.9,nl;q=0.8',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Cache-Control': 'no-cache',
+      'Pragma': 'no-cache',
+    },
   },
 
   projects: [
